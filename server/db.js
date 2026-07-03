05783b2be2cee9f7ebc290240b14636ada8aeb40
ah-shahin <ahmed.shahiin507@gmail.com>
2026-07-03 04:20:53 +0200

    Connect backend to Neon

--- server/db.js
+++ server/db.js
@@ -1,12 +1,11 @@
-require('dotenv').config();
-const { Pool } = require('pg');
+require("dotenv").config();
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: process.env.PGHOST,
-  port: process.env.PGPORT,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
